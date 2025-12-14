dea76aacd194a9608b8dd71cef156b8faf7a0aa2
Saulo Gatti <saulogatti@gmail.com>
2025-12-14 17:48:53 -0300

    first commit

---
+++ example/clean_archt_lint_example.dart
@@ -0,0 +1,6 @@
+import 'package:clean_archt_lint/clean_archt_lint.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/clean_archt_lint_test.dart
@@ -0,0 +1,16 @@
+import 'package:clean_archt_lint/clean_archt_lint.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
